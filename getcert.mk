dd45d8716b5c18da1e02dbf547b9443741c4b681
U-SASAKI64\sasaki <takashi.sasaki.mg@ehime-u.ac.jp>
2019-08-21 13:21:20 +0900

    CertUtil.exe -exportPFX

--- getcert.mk
+++ getcert.mk
@@ -2,15 +2,18 @@
 
 all: \
 	$(subst certhash,crt,$(wildcard *.certhash)) \
-	$(subst certhash,fingerprint,$(wildcard *.certhash)) 
+	$(subst certhash,fingerprint,$(wildcard *.certhash)) \
+	$(subst certhash,p12,$(wildcard *.certhash))  
 
 %.crt: %.certhash
-	CertUtil.exe -silent -privatekey -split -getcert $(basename $<) | iconv -f cp932
+	CertUtil.exe -silent -split -getcert $(basename $<) | iconv -f cp932
+
+%.p12: %.certhash
+	CertUtil.exe -privatekey -exportPfx $(basename $<) $(basename $<) | iconv -f cp932
 
 # So called 'CertHash' in CertUtil.exe is identical to OpenSSL SHA1 fingerprint
 %.fingerprint: %.crt
 	openssl x509 -fingerprint -inform DER -in $< -noout >$@
-
 
 clean:
 	git clean -fdx
